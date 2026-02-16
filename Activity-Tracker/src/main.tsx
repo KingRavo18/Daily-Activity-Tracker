675c36be16cc332558469a12534d534ce22d9a9d
KingRavo18 <rkingovskis14@gmail.com>
2026-02-16 12:57:30 +0200

    d

--- Activity-Tracker/src/main.tsx
+++ Activity-Tracker/src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './Style/index.css';
+import Connect from './Webpage-Connect/Connect';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Connect />
   </StrictMode>,
 )
