c9b11c6fd85ad25c04ecb7b6f998c121f6c96176
KingRavo18 <rkingovskis14@gmail.com>
2026-02-18 15:39:14 +0200

    e

--- Activity-Tracker/src/Webpage-Connect/Connect.tsx
+++ Activity-Tracker/src/Webpage-Connect/Connect.tsx
@@ -1,18 +1,21 @@
-
+import { useState } from "react";
 
 export default function Connect(){
+    const [username, setUsername] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
     async function HandleSignUp(): Promise<void>{
 
     }
 
     return(
-        <main>
+        <main className="connection_forms">
             <form onSubmit={HandleSignUp}>
                 <h1>Sign Up</h1>
-                <input type="username"/>
-                <input type="email"/>
-                <input type="password"/>
+                <input type="username" placeholder="Enter username:"/>
+                <input type="email" placeholder="Enter email:"/>
+                <input type="password" placeholder="Enter password:"/>
                 <input type="submit" value="Sign Up"/>
             </form>
         </main>
