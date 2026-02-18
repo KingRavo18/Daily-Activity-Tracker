36a871671d5b64b8639d1b9a06d23dede469f241
KingRavo18 <rkingovskis14@gmail.com>
2026-02-18 15:36:18 +0200

    d

--- Activity-Tracker/src/Webpage-Connect/Connect.tsx
+++ Activity-Tracker/src/Webpage-Connect/Connect.tsx
@@ -1,10 +1,20 @@
 
 
 export default function Connect(){
-    
-    return(
-        <div>
 
-        </div>
+    async function HandleSignUp(): Promise<void>{
+
+    }
+
+    return(
+        <main>
+            <form onSubmit={HandleSignUp}>
+                <h1>Sign Up</h1>
+                <input type="username"/>
+                <input type="email"/>
+                <input type="password"/>
+                <input type="submit" value="Sign Up"/>
+            </form>
+        </main>
     );
 }
