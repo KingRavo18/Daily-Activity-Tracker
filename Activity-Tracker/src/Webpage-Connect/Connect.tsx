503083d8ab34651b71c730360f00d4cc45b9bbaf
KingRavo18 <rkingovskis14@gmail.com>
2026-02-19 02:21:06 +0200

    s

--- Activity-Tracker/src/Webpage-Connect/Connect.tsx
+++ Activity-Tracker/src/Webpage-Connect/Connect.tsx
@@ -13,9 +13,9 @@ export default function Connect(){
         <main className="connection_forms">
             <form onSubmit={HandleSignUp}>
                 <h1>Sign Up</h1>
-                <input type="username" placeholder="Enter username:"/>
-                <input type="email" placeholder="Enter email:"/>
-                <input type="password" placeholder="Enter password:"/>
+                <input type="username" value={username} placeholder="Enter username:"/>
+                <input type="email" value={email} placeholder="Enter email:"/>
+                <input type="password" value={password} placeholder="Enter password:"/>
                 <input type="submit" value="Sign Up"/>
             </form>
         </main>
